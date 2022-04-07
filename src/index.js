632fbeb7476a01127d5d6898700d3509fe09fcac
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-04-07 09:58:03 -0400

    commented out line in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import GameView from "./scripts/game_view"
-const background = new Image();
-background.src = "../../images/background.png"
+// const background = new Image();
+// background.src = "../../images/background.png"
 
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById("game-canvas")
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     canvas.width = 1024;
     canvas.height = 768;
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
+    // ctx.drawImage(background, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
     ctx.font = "48px ArcadeClassic"
     ctx.fillStyle = "white";
     ctx.fillText("Press any key to start game", canvas.width / 2 - 275, canvas.height / 2);
